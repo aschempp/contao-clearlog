ce2828ea6f7c939dd45093e3fb5c065f1c3b1e7f
Kamil Kuzminski <kamil.kuzminski@gmail.com>
2013-07-24 18:56:42 +0200

    The extension is now compatible with Contao 3.1

--- config/config.php
+++ config/config.php
@@ -1,4 +1,4 @@
-<?php if (!defined('TL_ROOT')) die('You can not access this file directly!');
+<?php
 
 /**
  * Contao Open Source CMS
@@ -6,26 +6,15 @@
  *
  * Formerly known as TYPOlight Open Source CMS.
  *
- * This program is free software: you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation, either
- * version 3 of the License, or (at your option) any later version.
- * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- * 
- * You should have received a copy of the GNU Lesser General Public
- * License along with this program. If not, please visit the Free
- * Software Foundation website at <http://www.gnu.org/licenses/>.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
  *
  * PHP version 5
- * @copyright  Andreas Schempp 2008-2011
- * @author     Andreas Schempp <andreas@schempp.ch>
- * @author     Leo Unglaub <leo@leo-unglaub.net>
- * @license    http://opensource.org/licenses/lgpl-3.0.html
- * @version    $Id$
+ * @copyright  terminal42 gmbh 2009-2013
+ * @author     Andreas Schempp <andreas.schempp@terminal42.ch>
+ * @license    LGPL
  */
 
 
@@ -39,5 +28,5 @@ $GLOBALS['TL_CACHE'][] = 'tl_log';
  * Backend Modules
  */
 $GLOBALS['BE_MOD']['system']['log']['clearlog'] = array('tl_log_clearlog', 'clearLog');
-$GLOBALS['BE_MOD']['system']['log']['stylesheet'] = 'system/modules/clearlog/html/clearlog.css';
+$GLOBALS['BE_MOD']['system']['log']['stylesheet'] = 'system/modules/clearlog/assets/clearlog.css';
 
--- dca/tl_log.php
+++ dca/tl_log.php
@@ -1,4 +1,4 @@
-<?php if (!defined('TL_ROOT')) die('You can not access this file directly!');
+<?php
 
 /**
  * Contao Open Source CMS
@@ -6,26 +6,15 @@
  *
  * Formerly known as TYPOlight Open Source CMS.
  *
- * This program is free software: you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation, either
- * version 3 of the License, or (at your option) any later version.
- * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- * 
- * You should have received a copy of the GNU Lesser General Public
- * License along with this program. If not, please visit the Free
- * Software Foundation website at <http://www.gnu.org/licenses/>.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
  *
  * PHP version 5
- * @copyright  Andreas Schempp 2008-2011
- * @author     Andreas Schempp <andreas@schempp.ch>
- * @author     Leo Unglaub <leo@leo-unglaub.net>
- * @license    http://opensource.org/licenses/lgpl-3.0.html
- * @version    $Id$
+ * @copyright  terminal42 gmbh 2009-2013
+ * @author     Andreas Schempp <andreas.schempp@terminal42.ch>
+ * @license    LGPL
  */
 
 
@@ -37,7 +26,7 @@ $GLOBALS['TL_DCA']['tl_log']['config']['onload_callback'][] = array('tl_log_clea
 
 class tl_log_clearlog extends Backend
 {
-	
+
 	public function __construct()
 	{
 		parent::__construct();
@@ -57,7 +46,7 @@ class tl_log_clearlog extends Backend
 			);
 		}
 	}
-	
+
 	/**
 	 * Truncate table tl_log
 	 * @return void
@@ -69,7 +58,7 @@ class tl_log_clearlog extends Backend
 			$this->log('Back end module "maintenance" was not allowed for user "' . $this->User->username . '"', 'tl_log_clearlog clearLog()', TL_ERROR);
 			$this->redirect($this->Environment->script . '?act=error');
 		}
-		
+
 		$this->Database->query('TRUNCATE tl_log');
 		$this->redirect($this->Environment->script . '?do=log');
 	}
